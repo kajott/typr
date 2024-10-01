1b38ae96cc6873ce525e39ab6109db759ca5c9bc
Martin Fiedler <keyj@emphy.de>
2024-10-02 00:21:10 +0200

    update for Deadline 2024
    - new background
    - new font (+ keming tuning)
    - new colors
    - new logo color

--- config.js
+++ config.js
@@ -5,13 +5,13 @@ var Config = {
   replaceWords:0,
   replaceWordsWith:["bier", "kuchen", "panda"],
   textColor:0,
-  textColorPalette:["#ffffff","#f1ff4a","#ff3933", "#20b383"],
+  textColorPalette:["#eeffe4", "#ff0000", "#00ffff", "#f1ff4a", "#20b383"],
   textFont:0,
-  textFontList:["SpaceGroteskLight", "wingdings"],
+  textFontList:["Scale VF", "wingdings"],
   starfieldPalette:["#db2128", "#1a5dad", "#e0d9c9"],
   isScreenShakeEnabled:true,
   isPongEnabled:false,
   letterSpacingDividerClock: 10,
-  letterSpacingDividerText: 10,
+  letterSpacingDividerText: 20,
   normalWordSpacing:true
 };
